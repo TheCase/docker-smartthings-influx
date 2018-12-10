d24227cf7f26a99392ba3b45560e067bd80e3c1f
TheCase <case@repulsor.net>
2018-12-09 17:51:28 -0700

    new device capability format handling

--- smartthings.py
+++ smartthings.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
-import time, sys, os
+import time
+import sys
+import os
 import requests
 from influxdb import InfluxDBClient
 import datetime
@@ -8,93 +10,103 @@ import datetime
 from pprint import pprint
 
 # define the capabilities we want to capture
-capabilites = { "temperatureMeasurement":       "temperature",
-                "relativeHumidityMeasurement":  "humidity",
-                "battery":                      "battery",
-                "acceleration":                 "active",
-                "powerMeter":                   "power",  #watts
-                "energyMeter":                  "energy", #kWh
-                "accelerationSensor":            "acceleration"
-}
+capabilites = {"temperatureMeasurement":       "temperature",
+               "relativeHumidityMeasurement":  "humidity",
+               "battery":                      "battery",
+               "acceleration":                 "active",
+               "powerMeter":                   "power",  # watts
+               "energyMeter":                  "energy",  # kWh
+               "accelerationSensor":            "acceleration"
+               }
 
-def reformat(name,item_type):
-    name = name.replace(" ","_")
-    name = name.replace("/","-")
-    name = name.replace(":","")
-    for tag in ['Lock']:
-        if tag in item_type:
-            name = "{0}-{1}".format(name,tag)
-    return name.lower()
+
+def reformat(name, item_type):
+  name = name.replace(" ", "_")
+  name = name.replace("/", "-")
+  name = name.replace(":", "")
+  for tag in ['Lock']:
+    if tag in item_type:
+      name = "{0}-{1}".format(name, tag)
+  return name.lower()
+
 
 def convert(item):
-    if isinstance(item, basestring):
-        item = 0
-        if item == "active":
-            item = 1
-    return(item)
+  if isinstance(item, basestring):
+    item = 0
+    if item == "active":
+      item = 1
+  return(item)
+
 
 def influx(measurement, location, value):
-    client = InfluxDBClient(os.environ['INFLUXHOST'], os.environ['INFLUXPORT'], os.environ['INFLUXUSER'], os.environ['INFLUXPASS'], os.environ['INFLUXDB'])
-    json_body = [
-        {
-            "measurement": str(measurement),
-            "tags": {
-                "source": "zwave",
-                "location": str(location)
-            },
-            "fields": {
-                "value": float(value)
-            }
-        }
-    ]
-    client.write_points(json_body)
+  client = InfluxDBClient(os.environ['INFLUXHOST'], os.environ['INFLUXPORT'],
+                          os.environ['INFLUXUSER'], os.environ['INFLUXPASS'], os.environ['INFLUXDB'])
+  json_body = [
+      {
+          "measurement": str(measurement),
+          "tags": {
+              "source": "zwave",
+              "location": str(location)
+          },
+          "fields": {
+              "value": float(value)
+          }
+      }
+  ]
+  client.write_points(json_body)
+
 
 def get(url):
-    token  = os.environ['ST_TOKEN']
-    headers = { 'Authorization': 'Bearer {0}'.format(token) }
-    r = requests.get(url,headers=headers)
-    check_error(r)
-    data = r.json()
-    if ('_links' in data and 'next' in data['_links']):
-        print "fix this code for paging: https://smartthings.developer.samsung.com/develop/api-ref/st-api.html#section/Paging"
-        sys.exit(1)
-    return(data)
+  token = os.environ['ST_TOKEN']
+  headers = {'Authorization': 'Bearer {0}'.format(token)}
+  r = requests.get(url, headers=headers)
+  check_error(r)
+  data = r.json()
+  if ('_links' in data and 'next' in data['_links']):
+    print "fix this code for paging: https://smartthings.developer.samsung.com/develop/api-ref/st-api.html#section/Paging"
+    sys.exit(1)
+  return(data)
+
 
 def check_error(r):
-    if r.status_code != 200:
-        print("OAuth error: {0}\n{1}").format(r.status_code,r.text)
-        sys.exit(1)
+  if r.status_code != 200:
+    print("OAuth error: {0}\n{1}").format(r.status_code, r.text)
+    sys.exit(1)
+
 
 def push(metrics):
-    for item in metrics:
-        ts = datetime.datetime.now().isoformat()
-        (name,meas,value) = item.split(",")
-        print("{0} influx: {1} {2}: {3}").format(ts,name,meas,value)
-        influx(meas, name, value)
+  for item in metrics:
+    ts = datetime.datetime.now().isoformat()
+    (name, meas, value) = item.split(",")
+    print("{0} influx: {1} {2}: {3}").format(ts, name, meas, value)
+    influx(meas, name, value)
+
 
 def main():
-    metrics = list()
-    d_url = 'https://api.smartthings.com/v1/devices'
-    for device in get(d_url)['items']:
-        process = False
-        for cap in device['components'][0]['capabilities']:
-            if cap.values()[0] in capabilites:
-                process = True
-        if process:
-            url = '{0}/{1}/status'.format(d_url,device['deviceId'])
-            for cap,info in get(url)['components']['main'].iteritems():
-                if cap in capabilites:
-                    name = reformat(device['label'],device['name'])
-                    meas = capabilites[cap]
-                    value = convert(info[meas]['value'])
-                    ts = datetime.datetime.now().isoformat()
-                    print("{0} smartthings: {1} {2}: {3}").format(ts,name,meas,value)
-                    metrics.append("{0},{1},{2}".format(name,meas,value))
-    push(metrics)
+  metrics = list()
+  d_url = 'https://api.smartthings.com/v1/devices'
+  for device in get(d_url)['items']:
+    process = False
+    for cap in device['components'][0]['capabilities']:
+      if cap['id'] in capabilites:
+        process = True
+    if process:
+      url = '{0}/{1}/status'.format(d_url, device['deviceId'])
+      for cap, info in get(url)['components']['main'].iteritems():
+        if cap in capabilites:
+          name = reformat(device['label'], device['name'])
+          meas = capabilites[cap]
+          value = convert(info[meas]['value'])
+          ts = datetime.datetime.now().isoformat()
+          print("{0} smartthings: {1} {2}: {3}").format(ts, name, meas, value)
+          metrics.append("{0},{1},{2}".format(name, meas, value))
+  push(metrics)
+
 
 if __name__ == '__main__':
-    while True:
-        main()
-        ts = datetime.datetime.now().isoformat()
-        print("{0} sleeping for {1} seconds").format(ts,os.environ['POLL_INTERVAL'])
-        time.sleep(int(os.environ['POLL_INTERVAL']))
+  while True:
+    main()
+    ts = datetime.datetime.now().isoformat()
+    print("{0} sleeping for {1} seconds").format(
+        ts, os.environ['POLL_INTERVAL'])
+    time.sleep(int(os.environ['POLL_INTERVAL']))
